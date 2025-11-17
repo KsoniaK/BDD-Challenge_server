62e57935bc102508a27a109ba65cc4117c5a00a0
sonia kechit <kechits@yahoo.com>
2025-11-17 01:40:23 +0100

    Reset database

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("database2.sqlite");
+const db = new sqlite3.Database("database3.sqlite");
 
 // Création de la table + insertion données test
 db.serialize(() => {
